228324d47efa90f705f61483fcb26e16644ec347
Alena Alioshyna <elenakovalski1995@yandex.ru>
2025-11-26 17:05:33 +0300

    HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,38 @@
-import {UserType} from '../HW8'
+import {initialPeople, UserType} from '../HW8'
+
 
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
 
-            return state // need to fix
+export const homeWorkReducer = (state= initialPeople, action: ActionType): UserType[] => {
+    // need to
+    // fix any
+    switch (action.type) {
+        case 'sort': {
+            const sortedState = [...state]// by name
+            if (action.payload === 'up') {
+                return  sortedState.sort((a,b) => a.name > b.name ? 1 : -1)
+            } if (action.payload === 'down') {
+                return sortedState.sort((a,b) => a.name < b.name ? 1 : -1)
+            }
+           return sortedState
+            // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(s => s.age >= action.payload)// need to fix
         }
         default:
             return state
     }
 }
+
+export const sortUpAC = (): ActionType  => {
+    return {
+        type: 'sort',
+        payload: 'up'
+    } as const
+}
+
+
