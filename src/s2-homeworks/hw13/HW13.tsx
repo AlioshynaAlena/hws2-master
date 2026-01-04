e1982e527efd67f921343a3d5a8329be994544f1
Alena Alioshyna <elenakovalski1995@yandex.ru>
2026-01-04 22:06:31 +0300

    HW13_5 sprint_ change2

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -41,7 +41,7 @@ const HW13 = () => {
             .then((res) => {
                 setCode('Код 200!')
                 setImage(success200)
-                setText('... все ок)')
+                setText('...всё ок)')
                 setInfo(info)
                 // дописать
 
